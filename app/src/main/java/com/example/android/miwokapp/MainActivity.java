b2b65f29530b5d263799b5a6260c9d659afd10e5
Ravi Kumar <rkumardm@visa.com>
2018-01-24 19:25:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/android/miwokapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.android.miwokapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        TextView colors = (TextView) findViewById(R.id.colors);
+        TextView family = (TextView) findViewById(R.id.family);
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        Log.v("MainActivity", numbers.getText().toString());
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android/miwokapp/NumbersClickListener.java
@@ -0,0 +1,16 @@
+package com.example.android.miwokapp;
+
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by rkumardm on 20/01/18.
+ */
+
+public class NumbersClickListener implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {
+//        Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+    }
+}
